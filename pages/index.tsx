56424ef7f1727de0a7a074ff8ea1ad0b7989d715
s570908 <songohyoung@gmail.com>
2023-03-14 00:55:21 +0900

    #4 TAILWIND - 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,29 @@ import type { NextPage } from "next";
 import tw from "tailwind-styled-components";
 
 const Container = tw.div`
-  dark
-  flex
-  flex-col
+  grid
+  gap-10
   space-y-5
   bg-slate-400
   py-20
-  px-10 
+  px-20
+  min-h-screen
+  xl:grid-cols-3
 `;
 
 const Home: NextPage = () => {
   return (
-    <Container>
-      <div className="rounded-2xl bg-white p-6 shadow-2xl">
+    <div className="grid min-h-screen gap-10 space-y-5 bg-slate-400 px-20 py-20 lg:grid-cols-2 xl:grid-cols-3 xl:place-content-center">
+      <div className="flex flex-col justify-between rounded-2xl bg-white p-6 shadow-2xl">
         <span className="text-3xl font-semibold">Select Item</span>
-        <div className="my-2 flex justify-between">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
+        <ul>
+          {[1, 2].map((i) => (
+            <div key={i} className="my-2 flex justify-between">
+              <span className="text-gray-500">Grey Chair</span>
+              <span className="font-semibold">$19</span>
+            </div>
+          ))}
+        </ul>
         <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
           <span>Total</span>
           <span className="font-semibold">$10</span>
@@ -33,16 +34,16 @@ const Home: NextPage = () => {
         </button>
       </div>
       <div className="group overflow-hidden rounded-3xl bg-white shadow-xl">
-        <div className="bg-blue-500 p-6 pb-14">
+        <div className="bg-blue-500 p-6 pb-14 xl:pb-40 portrait:bg-indigo-500 landscape:bg-teal-400">
           <span className="text-2xl text-white">Profile</span>
         </div>
-        <div className="relative -top-5 rounded-3xl bg-white p-6">
+        <div className="group relative -top-5 rounded-3xl bg-white p-6">
           <div className="relative -top-16 flex items-end justify-between">
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 rounded-full bg-zinc-300 transition-colors group-hover:bg-red-300" />
+            <div className="h-24 w-24 rounded-full bg-gray-400 group-hover:bg-red-300"></div>
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Spent</span>
               <span className="font-medium">$2,310</span>
@@ -54,7 +55,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="rounded-2xl bg-white p-10 shadow-2xl">
+      <div className="rounded-2xl bg-white p-10 shadow-2xl lg:col-span-2 xl:col-span-1">
         <div className="mb-5 flex items-center justify-between">
           <span>⬅</span>
           <div className="space-x-3">
@@ -90,9 +91,11 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-    </Container>
+    </div>
   );
 };
+
+export default Home;
 
 /*
 
@@ -238,5 +241,3 @@ const Home: NextPage = () => {
       </form>
 
 */
-
-export default Home;
