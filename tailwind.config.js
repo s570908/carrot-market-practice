ad85eb19cf38fd40c3bd5e882c98efe421976b8d
s570908 <songohyoung@gmail.com>
2023-03-14 00:41:00 +0900

    #3 SETUP

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}", //어디서 사용할건지를 설정
+    "./components/**/*.{js,jsx,ts,tsx}", //어디서 사용할건지를 설정
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // class, media
+  plugins: [require("@tailwindcss/forms")], //npm i @tailwindcss/forms
+  variants: {
+    extend: {
+      backgroundColor: ["group-focus"],
+    },
+  },
+}; //<form>하면 기본적 form이 생성됨.
