6a52c5dac4868fecc0794bd086d9cf8d53112ccb
s570908 <songohyoung@gmail.com>
2023-03-14 19:05:58 +0900

    #5 TAILWIND ReFactoring UI - 1

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -1,87 +1,35 @@
+import Layout from "@components/Layout";
+import Message from "@components/Message";
 import type { NextPage } from "next";
 
-const CommunityPostDetail: NextPage = () => {
+const ChatDetail: NextPage = () => {
   return (
-    <div>
-      <span className="my-3 inline-flex items-center rounded-full bg-gray-100 px-4 py-0.5 text-xs font-medium text-gray-800">
-        동네질문
-      </span>
-      <div className="space-x-300 mb-3 flex items-center border-b px-3 py-2">
-        <div className="h-10 w-10 rounded-full bg-slate-300" />
-        <div>
-          <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
-          <p className="cursor-pointer text-xs font-medium text-gray-500">View profile &rarr;</p>
-        </div>
-      </div>
+    <Layout title="채팅" hasTabBar>
+      <div className="space-y-4 px-4 py-3">
+        <Message message="Hi how much are you selling them for?" />
+        <Message message="I want ￦20,000" reversed />
+        <Message message="미쳤어" />
+        <Message message="미쳤어" />
+        <Message message="I want ￦20,000" reversed />
+        <Message message="I want ￦20,000" reversed />
+        <Message message="Hi how much are you selling them for?" />
 
-      <div>
-        <div className="mt-2 px-4 text-gray-700">
-          <span className="font-medium text-orange-500">Q.</span> What is the best mandu restaurant?
-        </div>
-        <div className="mt-3 flex w-full space-x-5 border-t border-b-[2px] py-2.5 px-4 text-gray-700">
-          <div className="flex items-center space-x-2 text-sm">
-            <svg
-              className="h-4 w-4"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
-            <div>궁금해요 1</div>
-          </div>
-          <div className="flex items-center space-x-2 text-sm">
-            <svg
-              className="h-4 w-4"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-              ></path>
-            </svg>
-            <div>답변 1</div>
-          </div>
-        </div>
-      </div>
-
-      <div className="my-5 space-y-5 px-4">
-        {[...Array(3)].map((_, i) => (
-          <div key={i} className="flex items-start space-x-3">
-            <div className="h-8 w-8 rounded-full bg-slate-200" />
-            <div className="">
-              <span className="block text-sm font-medium text-gray-700">Steve Jebs</span>
-              <span className="block text-xs font-medium text-gray-500">2시간 전</span>
-              <p className="mt-[1.5px] text-gray-700">
-                The best mandu restaurant is the one next to my house.
-              </p>
+        <form className="fixed inset-x-0 bottom-20 bg-white py-2">
+          <div className="relative mx-auto flex w-full max-w-md items-center">
+            <input
+              type="text"
+              className="w-full rounded-full border-gray-300 pr-12 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+            />
+            <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+              <button className="flex items-center rounded-full bg-orange-500 px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
+                &rarr;
+              </button>
             </div>
           </div>
-        ))}
+        </form>
       </div>
-      <div className="px-4">
-        <textarea
-          rows={4}
-          placeholder="Answer this question!"
-          className="focus:border-1 mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-1 focus:ring-orange-500"
-        />
-        <button className="mt-2 w-full rounded-md border border-transparent bg-orange-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
-          Reply
-        </button>
-      </div>
-    </div>
+    </Layout>
   );
 };
 
-export default CommunityPostDetail;
+export default ChatDetail;
