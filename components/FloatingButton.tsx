a61f80a985267b540a2f73794293dcc2c5395522
s570908 <songohyoung@gmail.com>
2023-03-14 22:32:49 +0900

    refactor home.tsx 2

--- components/FloatingButton.tsx
+++ components/FloatingButton.tsx
@@ -1,12 +1,17 @@
+import Link from "next/link";
+
 interface FloatingButton {
   children: React.ReactNode;
+  href: string;
 }
 
-const FloatingButton = ({ children }: FloatingButton) => {
+const FloatingButton = ({ children, href }: FloatingButton) => {
   return (
-    <button className="fixed bottom-24 right-5 cursor-pointer rounded-full bg-orange-400 p-4 text-white shadow-xl transition-colors duration-300 hover:bg-orange-500">
-      {children}
-    </button>
+    <Link href={href}>
+      <a className="fixed bottom-24 right-5 cursor-pointer rounded-full bg-orange-400 p-4 text-white shadow-xl transition-colors duration-300 hover:bg-orange-500">
+        {children}
+      </a>
+    </Link>
   );
 };
 
