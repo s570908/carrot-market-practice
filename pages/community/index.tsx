eeaaccf02fae983641ea1abd4408aa193e95dafb
s570908 <songohyoung@gmail.com>
2023-03-16 08:32:15 +0900

    community 페이지를 refactor하였다.

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,3 +1,4 @@
+import FloatingButton from "@components/FloatingButton";
 import type { NextPage } from "next";
 
 const Community: NextPage = () => {
@@ -54,7 +55,7 @@ const Community: NextPage = () => {
           </div>
         </div>
       ))}
-      <button className="fixed bottom-24 right-5 cursor-pointer rounded-full bg-orange-400 p-4 text-white shadow-xl transition-colors duration-300 hover:bg-orange-500">
+      <FloatingButton href="/community/write">
         <svg
           className="h-6 w-6"
           fill="none"
@@ -69,7 +70,23 @@ const Community: NextPage = () => {
             d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
           ></path>
         </svg>
-      </button>
+      </FloatingButton>
+      {/* <button className="fixed p-4 text-white transition-colors duration-300 bg-orange-400 rounded-full shadow-xl cursor-pointer bottom-24 right-5 hover:bg-orange-500">
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+          ></path>
+        </svg>
+      </button> */}
     </div>
   );
 };
