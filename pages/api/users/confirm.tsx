f129a8e13c4415d5fcd110613ca21b53077ff45a
s570908 <songohyoung@gmail.com>
2023-04-09 17:28:05 +0900

    enter.tsx에 useForm<EnterForm>과 useForm<TokenForm>을 만들었다. useMutation을 api/users/enter 용과 api/users/confirm 용으로 두개를 만들었다. confirm을 위한 UI를 만들었다. data.ok가 true이면 confirm UI가 보이도록 하였다.

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -8,8 +8,9 @@ interface reqBodyType {
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse<ResponseType>) => {
   // const token = req.body.token;
   const { token }: reqBodyType = req.body;
-  console.log("confirm--token: ", req.body);
-  res.status(200).json({ ok: true, token }); // 주의! res.status(200).end({ ok: true, token })로 하면 안된다.
+  console.log("confirm--token: ", token);
+  res.status(200).json({ ok: true, token });
+  // 주의! res.status(200).end({ ok: true, token })로 하면 안된다. json 형태의 args는 .json()을 사용한다.
 };
 
 export default withHandler("POST", handler);
