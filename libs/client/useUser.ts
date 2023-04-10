e5999492d440541644c5b846dc70b47c5a058cbc
s570908 <songohyoung@gmail.com>
2023-04-11 01:00:28 +0900

    useUser에 useSWR을 사용하였다.

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,20 +1,31 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+// import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
+
+// export default function useUser() {
+//   const [user, setUser] = useState();
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     fetch("/api/users/me")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         if (data.ok) {
+//           setUser(data.profile);
+//         } else {
+//           router.replace("/enter");
+//         }
+//       });
+//   }, [router]);
+//   return user;
+// }
+import useSWR from "swr";
+
+const fetcher = async (url: string) => {
+  const response = await fetch(url);
+  return await response.json();
+};
 
 export default function useUser() {
-  const [user, setUser] = useState();
-  const router = useRouter();
-
-  useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.ok) {
-          setUser(data.profile);
-        } else {
-          router.replace("/enter");
-        }
-      });
-  }, [router]);
-  return user;
+  const { data, error } = useSWR("/api/users/me", fetcher);
+  return data;
 }
