aa29af99d6794f087a5f1da35423a29eba474961
s570908 <songohyoung@gmail.com>
2023-04-20 19:42:44 +0900

    profile/edit 페이지에 react-hook-form을 적용하였다.

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,31 +1,47 @@
 import Button from "@components/Button";
 import Input from "@components/Input";
-import Layout from "@components/Layout";
-import type { NextPage } from "next";
+import { NextPage } from "next";
+import { useForm } from "react-hook-form";
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { register, watch, handleSubmit, reset } = useForm<EditProfileForm>();
+  const onValid = (validForm: EditProfileForm) => {
+    const { email, phone } = validForm;
+    console.log("EditProfile--onValid, validForm :", validForm);
+  };
   return (
-    <Layout canGoBack>
-      <div className="space-y-4 px-4 py-3">
-        <div className="flex items-center space-x-3">
-          <div className="h-14 w-14 rounded-full bg-slate-400" />
-          <label
-            htmlFor="picture"
-            className="cursor-pointer rounded-md border border-gray-300 px-3 py-2 text-sm font-medium text-gray-700 shadow-sm focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-          >
-            Change photo
-            <input id="picture" type="file" className="hidden" accept="image/*"></input>
-          </label>
-        </div>
-        <div className="space-y-1">
-          <Input label="Email Adress" name="email" type="email" required />
-        </div>
-        <div className="space-y-1">
-          <Input label="Phone number" name="input" type="number" required />
-        </div>
-        <Button text="Update profile" />
+    <form onSubmit={handleSubmit(onValid)} className="space-y-4 px-4 py-10">
+      <div className="flex items-center space-x-3">
+        <div className="h-14 w-14 rounded-full bg-slate-400" />
+        <label
+          htmlFor="picture"
+          className="cursor-pointer rounded-md border border-gray-300 px-3 py-2 text-sm font-medium text-gray-700 shadow-sm focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+        >
+          Change photo
+          <input id="picture" type="file" className="hidden" accept="image/*"></input>
+        </label>
       </div>
-    </Layout>
+      <Input
+        register={register("email", { required: "Email is required." })}
+        label="Email Address"
+        name="email"
+        kind="text"
+        type="email"
+      />
+      <Input
+        register={register("phone", { required: "Phone number is required." })}
+        label="Phone number"
+        name="phone"
+        kind="phone"
+        type="number"
+      />
+      <Button text="Update profile" />
+    </form>
   );
 };
 
