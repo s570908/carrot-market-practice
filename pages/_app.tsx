4657cef2a8646b997d98e6df4800ea1b011589a3
s570908 <songohyoung@gmail.com>
2023-04-30 00:41:34 +0900

    Script Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 // import { SWRConfig } from "swr";
+import Script from "next/script";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -14,8 +15,27 @@ function MyApp({ Component, pageProps }: AppProps) {
     //     },
     //   }}
     // >    </SWRConfig>
-    <div className="mx-auto w-full max-w-xl">
-      <Component {...pageProps} />
+    <div>
+      <div className="mx-auto w-full max-w-xl">
+        <Component {...pageProps} />
+      </div>
+      <Script src="https://developers.kakao.com/sdk/js/kakao.js" strategy="lazyOnload" />
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        onLoad={() => {
+          console.log("facebook SDK loaded.");
+          // @ts-ignore
+          window.fbAsyncInit = function () {
+            // @ts-ignore
+            // FB.init({
+            //   appId: "your-app-id",
+            //   autoLogAppEvents: true,
+            //   xfbml: true,
+            //   version: "v14.0",
+            // });
+          };
+        }}
+      />
     </div>
   );
 }
