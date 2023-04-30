ef7e203cca0de2cc27a91485c58d25c1172fd7da
s570908 <songohyoung@gmail.com>
2023-04-30 13:01:47 +0900

    handler with try and catch

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,17 +1,16 @@
-import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // if (req.method !== "POST") {
-  //   res.status(401).end;
-  // }
-  console.log("req.body", req.body);
-  res.status(200).end();
+  if (req.method !== "POST") {
+    res.status(401).end;
+  }
+  try {
+    console.log("req.body", req.body);
+    res.status(200).end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
 }
-//export default handler;
 
-export default withHandler("POST", handler);
-// withHandler(...)는 function이다. 이 function은 ...
-// request method가 POST 가 아니면 res에 에러를 전송한다.
-// ...이면, handler를 수행시킨다. handler 수행 시에 catch(error)를
-// 할 수 있다.
+export default handler;
