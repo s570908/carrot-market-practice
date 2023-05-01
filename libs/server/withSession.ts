99c35edb76f704d0714f01895ca63d38acb335f1
s570908 <songohyoung@gmail.com>
2023-05-01 10:08:25 +0900

    SSR + Authentication: withIronSessionSsr를 이용한 withSsrSession을 만들었다. 이것을 profile 페이지에 적용하였다.

--- libs/server/withSession.ts
+++ libs/server/withSession.ts
@@ -1,4 +1,4 @@
-import { withIronSessionApiRoute } from "iron-session/next";
+import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
 
 declare module "iron-session" {
   interface IronSessionData {
@@ -15,4 +15,8 @@ const cookieOption = {
 
 export function withApiSession(fn: any) {
   return withIronSessionApiRoute(fn, cookieOption);
+}
+
+export function withSsrSession(handler: any) {
+  return withIronSessionSsr(handler, cookieOption);
 }
