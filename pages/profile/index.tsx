3ddfc4d78fff6966109ae1c1fe6486295bee2209
s570908 <songohyoung@gmail.com>
2023-05-07 01:31:59 +0900

    Profile 페이지에서 reviews를 보는 부분에 DB data를 가져와서 렌더링한다. useSWR("/api/reviews", fetcher)를 사용하였다.

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -4,11 +4,34 @@ import type { GetServerSidePropsContext, NextPage } from "next";
 import Link from "next/link";
 import client from "@libs/client/client";
 import useUser from "@libs/client/useUser";
-import { SWRConfig } from "swr";
-import { User } from "@prisma/client";
+import useSWR, { SWRConfig } from "swr";
+import { Review, User } from "@prisma/client";
+import { cls } from "@libs/utils";
+import fetcher from "@libs/client/fetcher";
+
+// interface ReviewsResponse {
+//   ok: boolean;
+//   reviews: (Review & {
+//     createdBy: {
+//       id: number;
+//       name: string;
+//       avatar: string | null;
+//     };
+//   })[];
+// }
+
+interface ReviewsWithUser extends Review {
+  createdBy: User;
+}
+interface ReviewsResponse {
+  ok: boolean;
+  reviews: ReviewsWithUser[];
+}
 
 const Profile: NextPage = () => {
   const { user } = useUser();
+  const { data } = useSWR<ReviewsResponse>("/api/reviews", fetcher);
+  console.log("Profile--useSWR-/api/reviews, data: ", data);
   return (
     <Layout title="나의 계정" hasTabBar>
       <div className="px-4 py-3">
@@ -88,70 +111,40 @@ const Profile: NextPage = () => {
             </a>
           </Link>
         </div>
-        <div className="mt-12">
-          <div className="flex items-center space-x-4">
-            <div className="h-12 w-12 rounded-full bg-slate-400" />
-            <div>
-              <h4 className="text-sm font-bold text-gray-800">니꼬</h4>
-              <div className="flex items-center">
-                <svg
-                  className="h-5 w-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-                <svg
-                  className="h-5 w-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-                <svg
-                  className="h-5 w-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-                <svg
-                  className="h-5 w-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-                <svg
-                  className="h-5 w-5 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
+        {data?.reviews.map((review) => {
+          return (
+            <div key={review.id} className="mt-12">
+              <div className="flex items-center space-x-4">
+                <div className="h-12 w-12 rounded-full bg-slate-400" />
+                <div>
+                  <h4 className="text-sm font-bold text-gray-800">{review.createdBy.name}</h4>
+                  <div className="flex items-center">
+                    {[1, 2, 3, 4, 5].map((star) => {
+                      return (
+                        <svg
+                          key={star}
+                          className={cls(
+                            "h-5 w-5",
+                            review.score >= star ? "text-yellow-400" : "text-gray-400"
+                          )}
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                        </svg>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div className="mt-4 text-sm text-gray-600">
+                <p>{review.review}</p>
               </div>
             </div>
-          </div>
-          <div className="mt-4 text-sm text-gray-600">
-            <p>
-              Normally, both your asses would be dead as fucking fried chicken, but you happen to
-              pull this shit while I&apos;m in a transitional period so I don&apos;t wanna kill you,
-              I wanna help you. But I can&apos;t give you this case, it don&apos;t belong to me.
-              Besides, I&apos;ve already been through too much shit this morning over this case to
-              hand it over to your dumb ass.
-            </p>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </Layout>
   );
@@ -187,3 +180,5 @@ export const getServerSideProps = withSsrSession(async ({ req }: GetServerSidePr
 });
 
 export default Page;
+
+//export default Profile;
