3e589c1b5a4900fb1eb056f629ce10fcac685f71
s570908 <songohyoung@gmail.com>
2023-05-06 18:02:51 +0900

    스키마에서 phone 을 String으로 변경하였고,  enter.tsx: <Input>에서 phone에 관해서는 type을 'tel'로 변경하였다. 스키마에 model Review를 만들어 넣었다. /api/review에 GET과 PUT request를 구현하였다.

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -16,7 +16,7 @@ interface reqBodyType {
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse<ResponseType>) => {
   const { email, phone }: reqBodyType = req.body;
   console.log("req.body", req.body);
-  const userKey = phone ? { phone: +phone } : { email: email };
+  const userKey = phone ? { phone: phone } : { email: email };
 
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
 
@@ -45,18 +45,22 @@ const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse
   });
 
   console.log("api/enter--token: ", token);
-  if (phone) {
-    const message = await twilioClient.messages.create({
-      messagingServiceSid: process.env.TWILIO_MSID,
-      // 원래라면 phone으로 보내야 하지만 -> dev process에서는 그냥 내 폰으로
-      to: process.env.MY_PHONE!,
-      body: `Your login token is ${payload}`,
-    });
+  /***********************************************************
+   *  아래의 과정으로 토큰을 사용자에게 전송한다. phone sms 는 twilio를 사용하였으나 정해진 한도를 넘으면 유료이다.
+   *  naver sense를 사용할 수 있다. 한달에 50건이 무료이다. 개발 중에는 서버 로그를 보면 token.payload에 토큰이 들어 있다. 이것을 활용한다.
+   */
+  // if (phone) {
+  //   const message = await twilioClient.messages.create({
+  //     messagingServiceSid: process.env.TWILIO_MSID,
+  //     // 원래라면 phone으로 보내야 하지만 -> dev process에서는 그냥 내 폰으로
+  //     to: process.env.MY_PHONE!,
+  //     body: `Your login token is ${payload}`,
+  //   });
 
-    //console.log(message);
-  } else if (email) {
-    await sendTokenEmail(token.user.name, payload);
-  }
+  //   //console.log(message);
+  // } else if (email) {
+  //   await sendTokenEmail(token.user.name, payload);
+  // }
   res.status(200).json({ ok: true });
 };
 
