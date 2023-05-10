8f86f5b69da9f1fb5168f8bfe3d06bf28813960c
s570908 <songohyoung@gmail.com>
2023-05-10 23:49:41 +0900

    ISR on demand 방식 예제: https://velog.io/@seungchan__y/NextJS%EC%99%80-ISR

--- NOTE.md
+++ NOTE.md
@@ -1,10 +1,9 @@
 # Why This Error Occurred
 
-Next.js 13버전이 나오면서, Link 태그는<a>로 렌더링 되므로 <a>를 사용하는 시도는 유효하지 않다고 하네요!
+- Next.js 13버전이 나오면서, Link 태그는`<a>`로 렌더링 되므로 `<a>`를 사용하는 시도는 유효하지 않다고 하네요!
 
-How to solve the problem
-
-npx @next/codemod new-link .
+- How to solve the problem: <br />
+  npx @next/codemod new-link .
 
 # how to remove firefox's Default Dropdown for input html
 
@@ -13,3 +12,26 @@ https://stackoverflow.com/questions/71296535/how-to-remove-arrow-on-input-type-n
 # How to Remove Arrow on Input type Number with Tailwind CSS
 
 https://stackoverflow.com/questions/71296535/how-to-remove-arrow-on-input-type-number-with-tailwind-css
+
+# Data Fetching에 대해 알아보기 (3) - ISR
+
+https://jforj.tistory.com/m/314
+
+# NextJS와 ISR
+
+https://velog.io/@seungchan__y/NextJS%EC%99%80-ISR
+
+1. /libs/server/db.json에 update 혹은 create 가 발생하였고 이것을 알게된 webhook 이나 알림서비스가 POST request를 /api/revalidate-book 에 보낸다
+
+2. 즉,....
+
+```js
+POST http://localhost:3000/api/revalidate-books?secret=nalnari
+Content-Type: application/json
+
+{
+  "id": "9"
+}
+```
+
+3. /api/revalidate-book 핸들러는 authentication을 수행하여 정당한 request인가를 확인한 후에 ISR을 수행하여서 cache를 업데이트한다.
--- pages/api/revalidate-books.ts
+++ pages/api/revalidate-books.ts
@@ -1,7 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+const SECRET_REVALIDATE_TOKEN = "nalnari";
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method, query, body } = req;
+  console.log("method, query, body: ", method, query, body);
 
   if (method !== "POST") {
     return res.status(400).json({ error: "Invalid HTTP method. Only POST method is allowed." });
@@ -12,15 +15,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   //   return res.status(401).json({ message: "Invalid token" });
   // }
 
-  try {
-    if (!body) {
-      res.status(400).send("Bad reqeust (no body)");
-      return;
-    }
+  // Unauthorized access as invalid token
+  if (query.secret !== SECRET_REVALIDATE_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 
+  if (!body) {
+    return res.status(400).send("Bad reqeust (no body)");
+  }
+
+  try {
     const idToRevalidate = body.id;
 
     if (idToRevalidate) {
+      // this should be the actual path not a rewritten path
+      // e.g. for "/blog/[slug]" this should be "/blog/post-1"
       await res.revalidate("/books");
       await res.revalidate(`/books/${idToRevalidate}`);
       return res.json({ revalidated: true });
