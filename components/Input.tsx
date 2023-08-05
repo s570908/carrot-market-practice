e4ba94fd65e7179848574ff48356c258b432384a
s570908 <songohyoung@gmail.com>
2023-08-05 23:02:06 +0900

    post api/user/me 연습

--- components/Input.tsx
+++ components/Input.tsx
@@ -6,11 +6,12 @@ interface InputProps {
   kind?: "text" | "phone" | "price";
   register: UseFormRegisterReturn;
   type: string;
+  placeholder?: string;
   // [key: string]: any;
 }
 
 /* defualt is kind="text" */
-const Input = ({ kind = "text", name, label, register, type }: InputProps) => {
+const Input = ({ kind = "text", name, label, register, type, placeholder }: InputProps) => {
   return (
     <div>
       <label htmlFor={name} className="text-sm font-medium text-gray-700">
@@ -50,7 +51,7 @@ const Input = ({ kind = "text", name, label, register, type }: InputProps) => {
               {...register}
               className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 pl-7 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
               type={type}
-              placeholder="0.00"
+              placeholder={placeholder}
               step="0.01"
             />
             <div className="pointer-events-none absolute right-0 flex items-center pr-3">
@@ -64,3 +65,78 @@ const Input = ({ kind = "text", name, label, register, type }: InputProps) => {
 };
 
 export default Input;
+
+/* 
+
+import type { UseFormRegisterReturn } from "react-hook-form";
+
+interface InputProps {
+  label: string;
+  name: string;
+  kind?: "text" | "phone" | "price";
+  type: string;
+  register: UseFormRegisterReturn;
+  placeholder?: string;
+  value?: string;
+}
+
+export default function Input({
+  label,
+  name,
+  kind = "text",
+  register,
+  type,
+  placeholder,
+  value,
+}: InputProps) {
+  return (
+    <div>
+      <label className="block mb-1 text-sm font-medium text-gray-700" htmlFor={name}>
+        {label}
+      </label>
+      {kind === "text" ? (
+        <div className="relative flex items-center rounded-md shadow-sm">
+          <input
+            id={name}
+            {...register}
+            type={type}
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+            value={value}
+          />
+        </div>
+      ) : null}
+      {kind === "price" ? (
+        <div className="relative flex items-center rounded-md shadow-sm">
+          <div className="absolute left-0 flex items-center justify-center pl-3 pointer-events-none">
+            <span className="text-sm text-gray-500">￦</span>
+          </div>
+          <input
+            id={name}
+            {...register}
+            type={type}
+            placeholder={placeholder}
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none pl-7 focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+          />
+          <div className="absolute right-0 flex items-center pr-3 pointer-events-none">
+            <span className="text-gray-500">KRW</span>
+          </div>
+        </div>
+      ) : null}
+      {kind === "phone" ? (
+        <div className="flex rounded-md shadow-sm">
+          <span className="flex items-center justify-center px-3 text-sm text-gray-500 border border-r-0 border-gray-300 select-none rounded-l-md bg-gray-50">
+            +82
+          </span>
+          <input
+            id={name}
+            {...register}
+            type={type}
+            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md rounded-l-none shadow-sm appearance-none focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+          />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+ */
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,5 +1,76 @@
 import Button from "@components/Button";
 import Input from "@components/Input";
+import Layout from "@components/Layout";
+import useMutation from "@libs/client/useMutation";
+import type { NextPage } from "next";
+import { useForm } from "react-hook-form";
+
+interface EditprofileForm {
+  name?: string;
+  phone?: string;
+  email?: string;
+}
+
+const EditProfile: NextPage = () => {
+  const { register, handleSubmit } = useForm<EditprofileForm>();
+  const [editProfile, { data, loading, error }] = useMutation("/api/users/me");
+
+  const onValid = (validForm: EditprofileForm) => {
+    const { name, email, phone } = validForm;
+    console.log("EditProfile----name, email, phone: ", name, email, phone);
+    editProfile({ name, email, phone });
+  };
+
+  return (
+    <Layout canGoBack>
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 px-4 py-3">
+        <div className="flex items-center space-x-3">
+          <div className="h-14 w-14 rounded-full bg-slate-400" />
+          <label
+            htmlFor="picture"
+            className="cursor-pointer rounded-md border border-gray-300 px-3 py-2 text-sm font-medium text-gray-700 shadow-sm focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+          >
+            Change photo
+            <input id="picture" type="file" className="hidden" accept="image/*"></input>
+          </label>
+        </div>
+        <div className="space-y-1">
+          <Input
+            register={register("name", { required: "Name is required." })}
+            label="Name"
+            name="name"
+            type="text"
+          />
+        </div>
+        <div className="space-y-1">
+          <Input
+            register={register("email", { required: "Email is required." })}
+            label="Email Adress"
+            name="email"
+            type="email"
+          />
+        </div>
+        <div className="space-y-1">
+          <Input
+            register={register("phone", { required: "Phone number is required." })}
+            label="Phone number"
+            name="input"
+            type="number"
+            kind="phone"
+          />
+        </div>
+        <Button text="Update profile" />
+      </form>
+    </Layout>
+  );
+};
+
+export default EditProfile;
+
+/* 
+import Button from "@components/Button";
+import Input from "@components/Input";
+import Layout from "@components/Layout";
 import useMutation from "@libs/client/useMutation";
 import { NextPage } from "next";
 import { useForm } from "react-hook-form";
@@ -19,41 +90,44 @@ const EditProfile: NextPage = () => {
     editProfile({ email, phone, name });
   };
   return (
-    <form onSubmit={handleSubmit(onValid)} className="space-y-4 px-4 py-10">
-      <div className="flex items-center space-x-3">
-        <div className="h-14 w-14 rounded-full bg-slate-400" />
-        <label
-          htmlFor="picture"
-          className="cursor-pointer rounded-md border border-gray-300 px-3 py-2 text-sm font-medium text-gray-700 shadow-sm focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-        >
-          Change photo
-          <input id="picture" type="file" className="hidden" accept="image/*"></input>
-        </label>
-      </div>
-      <Input
-        register={register("name", { required: "Name is required." })}
-        label="Name"
-        name="name"
-        kind="text"
-        type="text"
-      />
-      <Input
-        register={register("email", { required: "Email is required." })}
-        label="Email Address"
-        name="email"
-        kind="text"
-        type="email"
-      />
-      <Input
-        register={register("phone", { required: "Phone number is required." })}
-        label="Phone number"
-        name="phone"
-        kind="phone"
-        type="number"
-      />
-      <Button text="Update profile" />
-    </form>
+    <Layout canGoBack title="Edit Profile">
+      <form onSubmit={handleSubmit(onValid)} className="px-4 py-10 space-y-4">
+        <div className="flex items-center space-x-3">
+          <div className="rounded-full h-14 w-14 bg-slate-400" />
+          <label
+            htmlFor="picture"
+            className="px-3 py-2 text-sm font-medium text-gray-700 border border-gray-300 rounded-md shadow-sm cursor-pointer focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+          >
+            Change photo
+            <input id="picture" type="file" className="hidden" accept="image/*"></input>
+          </label>
+        </div>
+        <Input
+          register={register("name", { required: "Name is required." })}
+          label="Name"
+          name="name"
+          kind="text"
+          type="text"
+        />
+        <Input
+          register={register("email", { required: "Email is required." })}
+          label="Email Address"
+          name="email"
+          kind="text"
+          type="email"
+        />
+        <Input
+          register={register("phone", { required: "Phone number is required." })}
+          label="Phone number"
+          name="phone"
+          kind="phone"
+          type="number"
+        />
+        <Button text="Update profile" />
+      </form>
+    </Layout>
   );
 };
 
-export default EditProfile;
+export default EditProfile; 
+*/
