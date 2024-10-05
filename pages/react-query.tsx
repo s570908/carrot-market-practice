b32e1ad72de782d0b2291208ec86b84fe4aec83b
s570908 <kko0831@hanmail.net>
2024-10-05 22:28:42 +0900

    React Query 강좌 3편. 클릭시 fetch하는 방법과 커스텀 콜백함수 작성, useQuery에서 데이터 변환, 커스텀 훅 만들기 - 2. 커스텀 콜백 함수 만들기 (onSuccess, onError)

--- pages/react-query.tsx
+++ pages/react-query.tsx
@@ -19,9 +19,18 @@ const fetchProducts = () => {
 };
 
 const ReactQuery = () => {
+  const onSuccess = (data: ProductResponse) => {
+    console.log("데이터 가져오기 후 사이드 이펙트 수행", data);
+  };
+
+  const onError = (error: unknown) => {
+    console.log("오류 발생 후 사이드 이펙트 수행", error);
+  };
+
   const { isLoading, isFetching, data, isError, error, refetch } =
     useQuery<ProductResponse>("get-product", fetchProducts, {
-      enabled: false,
+      onSuccess,
+      onError,
     });
 
   console.log({ isLoading, isFetching });
@@ -37,7 +46,7 @@ const ReactQuery = () => {
       <TestHeader />
       <div className="text-4xl">ReactQuery</div>
       <button
-        onClick={refetch}
+        onClick={() => refetch}
         className="rounded-md border bg-slate-100 px-4 py-2"
       >
         fetch data
