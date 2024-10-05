51d3377885252de989957f9f154fe84d937a9043
s570908 <kko0831@hanmail.net>
2024-10-05 13:19:06 +0900

    React Query 강좌 3편. 클릭시 fetch하는 방법과 커스텀 콜백함수 작성, useQuery에서 데이터 변환, 커스텀 훅 만들기 - 1. 유저 클릭시 useQuery 작동시키기

--- pages/react-query.tsx
+++ pages/react-query.tsx
@@ -19,10 +19,9 @@ const fetchProducts = () => {
 };
 
 const ReactQuery = () => {
-  const { isLoading, isFetching, data, isError, error } =
+  const { isLoading, isFetching, data, isError, error, refetch } =
     useQuery<ProductResponse>("get-product", fetchProducts, {
-      refetchInterval: 2000,
-      refetchIntervalInBackground: true,
+      enabled: false,
     });
 
   console.log({ isLoading, isFetching });
@@ -37,6 +36,12 @@ const ReactQuery = () => {
     <>
       <TestHeader />
       <div className="text-4xl">ReactQuery</div>
+      <button
+        onClick={refetch}
+        className="rounded-md border bg-slate-100 px-4 py-2"
+      >
+        fetch data
+      </button>
       <ul className="list-disc p-4">
         {data &&
           data.data?.items?.map((product: Product) => (
