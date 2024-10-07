09c4ffeb8b20ddb68e0434f449871c06d7b809c3
s570908 <kko0831@hanmail.net>
2024-10-07 11:37:02 +0900

    React Query 강좌 3편. 클릭시 fetch하는 방법과 커스텀 콜백함수 작성, useQuery에서 데이터 변환, 커스텀 훅 만들기 - 4. useQuery를 커스텀 훅으로 만들어서 재사용하기

---
+++ libs/client/useProductName.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+}
+
+interface ProductResponse {
+  data: {
+    items: Product[];
+  };
+}
+
+const fetchProducts = () => {
+  return axios.get("http://localhost:4000/data");
+};
+
+export const useProductName = (
+  onSuccess: (data: any) => void,
+  onError: (error: unknown) => void
+) => {
+  return useQuery("get-product", fetchProducts, {
+    onSuccess: onSuccess,
+    onError: onError,
+    select: (data: ProductResponse): any => {
+      const productName = data?.data?.items
+        ?.filter((p: Product) => p.price <= 9)
+        .map((p: Product) => p?.name);
+      return productName;
+    },
+  });
+};
--- pages/react-query.tsx
+++ pages/react-query.tsx
@@ -1,22 +1,7 @@
 import { useQuery } from "react-query";
 import axios, { AxiosError } from "axios";
 import { TestHeader } from "@components/TestHeader";
-
-interface Product {
-  id: string;
-  name: string;
-  price: number;
-}
-
-interface ProductResponse {
-  data: {
-    items: Product[];
-  };
-}
-
-const fetchProducts = () => {
-  return axios.get("http://localhost:4000/data");
-};
+import { useProductName } from "@libs/client/useProductName";
 
 const ReactQuery = () => {
   const onSuccess = (data: any) => {
@@ -27,19 +12,9 @@ const ReactQuery = () => {
     console.log("오류 발생 후 사이드 이펙트 수행", error);
   };
 
-  const { isLoading, isFetching, data, isError, error } = useQuery(
-    "get-product",
-    fetchProducts,
-    {
-      onSuccess: onSuccess,
-      onError: onError,
-      select: (data: ProductResponse): any => {
-        const productName = data?.data?.items
-          ?.filter((p: Product) => p.price <= 9)
-          .map((p: Product) => p?.name);
-        return productName;
-      },
-    }
+  const { isLoading, isFetching, data, isError, error } = useProductName(
+    onSuccess,
+    onError
   );
 
   console.log({
